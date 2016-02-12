6bf06c471d43e702497d846682bc5c2b0ad92f8f
levent93 <mustafa.levent.27.12@ict.edu.rs>
2016-02-12 20:39:53 +0100

    Konju jedan! Sta koj moj menjas naziv projekta??!?!!?
    i kad pozivas css i js kazes nema base_url??? pjederu jedan

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CI_27_12_2/';
+$config['base_url'] = 'http://localhost/GitHub/Blog';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/footer.php
+++ application/views/footer.php
@@ -14,10 +14,10 @@
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
 
 	</body>
 
